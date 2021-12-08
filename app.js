43a2cf196cd627969785c9b4294bd832954f3295
C Davis <cdavis@joegalaxy.com>
2021-12-08 16:19:13 -0600

    add & configure eslintrc.json & .prettierrc

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // 1. Middleware
-if(process.env.NODE_ENV === 'development') {
-    app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
 }
 app.use(express.json());
 app.use(express.static(`${__dirname}/starter/public`));
@@ -17,17 +17,17 @@ app.use(express.static(`${__dirname}/starter/public`));
 //Our own middleware.
 // The order of the middlewares below matters.
 app.use((req, res, next) => {
-    console.log('Hello from the middleware 😎');
-    next();
+  console.log('Hello from the middleware 😎');
+  next();
 });
 
 //middleware creating a requestTime value.
 app.use((req, res, next) => {
-    req.requestTime = new Date().toISOString();
-    next();
-})
+  req.requestTime = new Date().toISOString();
+  next();
+});
 
-//tourRouter, userRouter middleware 
+//tourRouter, userRouter middleware
 // mounting routers.
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/tours', tourRouter);
--- server.js
+++ server.js
@@ -1,17 +1,13 @@
 const dotenv = require('dotenv');
-
 //configuration file for environment variables
-dotenv.config({ path: './config.env'});
-
+dotenv.config({ path: './config.env' });
 //importing app
 const app = require('./app');
-
 
 //console.log(process.env);
 
 // Start Server.
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`App running on port ${port}...`);
+  console.log(`App running on port ${port}...`);
 });
-
