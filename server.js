49948266b7c5fea6c66afa09ba5b4f16723fa235
C Davis <cdavis@joegalaxy.com>
2021-12-09 18:49:45 -0600

    added connection to local & remote DB plus modified scripts

--- server.js
+++ server.js
@@ -1,10 +1,30 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 //configuration file for environment variables
-dotenv.config({ path: './config.env' });
 //importing app
 const app = require('./app');
 
-//console.log(process.env);
+dotenv.config({ path: './config.env' });
+
+// create DB var by getting config.env
+// DB password & replacing placeholder.
+const DB = process.env.DATABASE.replace(
+    '<PASSWORD>',
+    process.env.DATABASE_PASSWORD
+);
+
+//extra fields to deal with deprecation warnings.
+// just use them in your apps.
+mongoose
+    //Local mongoDB connection.
+    //.connect(process.env.DATABASE_LOCAL, {
+    // Atlas remote DB
+    .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+})
+.then(() => console.log('DB connection successful!'));
 
 // Start Server.
 const port = process.env.PORT || 3000;
