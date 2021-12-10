5f4891ea405f926c2a854188a1e70ad9456edc9e
C Davis <cdavis@joegalaxy.com>
2021-12-10 17:27:45 -0600

    updated createTour Controller to return err, updated tourSchema

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -107,7 +107,7 @@ exports.createTour = async (req, res) => {
     }catch(err) {
         res.status(404).json({
             status: 'fail',
-            message: 'Invalid data sent!'
+            message: err
         })
     }
     
