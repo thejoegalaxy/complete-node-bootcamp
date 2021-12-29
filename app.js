5ff83424c592f4dfeb5068b9ff1baa092a721890
C Davis <cdavis@joegalaxy.com>
2021-12-28 19:36:17 -0600

    extended our base template with blocks & added overview & tour routes

--- app.js
+++ app.js
@@ -94,6 +94,18 @@ app.get('/', (req, res) => {
   }); //express will look for the template base in the views folder.
 });
 
+app.get('/overview', (req, res) => {
+  res.status(200).render('overview', {
+    title: 'All Tours',
+  });
+});
+
+app.get('/tour', (req, res) => {
+  res.status(200).render('tour', {
+    title: 'The Forest Hiker Tour',
+  });
+});
+
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
