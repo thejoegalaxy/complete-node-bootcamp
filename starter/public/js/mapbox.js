32f680e997f299ac9ab4228501ddb8a99a6af978
C Davis <cdavis@joegalaxy.com>
2021-12-29 17:40:03 -0600

    added mapbox.js to include a Map on the Tour page part 1

--- starter/public/js/mapbox.js
+++ starter/public/js/mapbox.js
@@ -1,1 +1,5 @@
-console.log('hello from the client side:D');
+/* eslint-disable */
+// grabbing the data-locations from our map element from tour.pug map section.
+// we converted the locations data to a string and then here back to JSON.
+const locations = JSON.parse(document.getElementById('map').dataset.locations);
+console.log(locations);
