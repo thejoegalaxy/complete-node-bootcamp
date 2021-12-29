0c663b9fd18a3b272bdad838d8150e049ae71034
C Davis <cdavis@joegalaxy.com>
2021-12-28 21:18:06 -0600

    added the pug code for a card container building the tour overview part 1

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,8 +1,20 @@
-exports.getOverview = (req, res) => {
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
+
+exports.getOverview = catchAsync(async (req, res, next) => {
+  // 1) Get tour data from collection
+  const tours = await Tour.find();
+
+  // 2) Build Template
+  // done in overview.pug
+
+  // Render that template using tour data from 1).
   res.status(200).render('overview', {
     title: 'All Tours',
+    tours, //pass tours into render overview.
   });
-};
+});
 
 exports.getTour = (req, res) => {
   res.status(200).render('tour', {
