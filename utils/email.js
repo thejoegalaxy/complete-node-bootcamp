97eac04eacad8d05db54c89049349d5c384db388
C Davis <cdavis@joegalaxy.com>
2022-01-08 18:17:58 -0600

    complex email handler & email templates with pug: welcome emails

--- utils/email.js
+++ utils/email.js
@@ -1,45 +1,45 @@
 const nodemailer = require('nodemailer');
 const pug = require('pug');
-const htmlToText = require('html-to-text');
+const { htmlToText } = require('html-to-text');
+
 //new Email(user, url).sendWelcome();
 
 module.exports = class Email {
   constructor(user, url) {
     this.to = user.email;
-    this.firstname = user.name.split(' ')[0];
+    this.firstName = user.name.split(' ')[0];
     this.url = url;
     this.from = `C Davis <${process.env.EMAIL_FROM}>`;
   }
 
-  newTransport() {
-    if (process.env.NODE_ENV !== 'production') {
+  newTransport(mailOptions) {
+    if (process.env.NODE_ENV === 'production') {
       //sendgrid
       return 1;
     }
 
-    return nodemailer.createTransport({
-      //service: 'Gmail',
-      host: process.env.EMAIL_HOST,
-      port: process.env.EMAIL_PORT,
-      auth: {
-        user: process.env.EMAIL_USERNAME,
-        pass: process.env.EMAIL_PASSWORD,
-      },
-      //Activate in gmail "less secure app" option. Send grid better.
-    });
+    return nodemailer
+      .createTransport({
+        //service: 'Gmail',
+        host: process.env.EMAIL_HOST,
+        port: process.env.EMAIL_PORT,
+        auth: {
+          user: process.env.EMAIL_USERNAME,
+          pass: process.env.EMAIL_PASSWORD,
+        },
+        //Activate in gmail "less secure app" option. Send grid better.
+      })
+      .sendMail(mailOptions); //had to put this here, wasn't working below.
   }
 
   async send(template, subject) {
     //send the actual email
     // 1 Render html based on a pug template.
-    const html = pug.renderFile(
-      `${__dirname}/../views/emails/${template}.pug`,
-      {
-        firstName: this.firstName,
-        url: this.url,
-        subject,
-      }
-    );
+    const html = pug.renderFile(`${__dirname}/../views/email/${template}.pug`, {
+      firstName: this.firstName,
+      url: this.url,
+      subject,
+    });
 
     // 2. Define email options.
     //2. Define the email Options
@@ -53,7 +53,8 @@ module.exports = class Email {
     };
 
     //3. create a transport and send email.
-    await this.newTransport().sendMail(mailOptions);
+    await this.newTransport(mailOptions);
+    //await this.newTransport().sendMail(mailOptions);
   }
 
   async sendWelcome() {
