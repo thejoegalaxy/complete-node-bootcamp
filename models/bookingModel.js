3f5156b034dd9312a6542dca46ccebf9d4ea6075
C Davis <cdavis@joegalaxy.com>
2022-01-10 23:46:48 -0600

    added bookingModel

---
+++ models/bookingModel.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+const bookingSchema = new mongoose.Schema({
+  tour: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'Tour',
+    required: [true, 'Booking must belong to a Tour!'],
+  },
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'User',
+    require: [true, 'Booking must belong to a User!'],
+  },
+  price: {
+    type: Number,
+    require: [true, 'Booking must have a price.'],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  paid: {
+    type: Boolean,
+    default: true,
+  },
+});
+
+//prepoluating user & tour name when any kind of find.
+bookingSchema.pre(/^find/, function (next) {
+  this.populate('user').populate({
+    path: 'tour',
+    select: 'name', //only selecting name from tour.
+  });
+});
+//Booking model creation.
+const Booking = mongoose.model('Booking', bookingSchema);
+
+module.exports = Booking;
