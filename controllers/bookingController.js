bd9caee8affc3a3c652369ce4d564ff0ad9ea47b
C Davis <cdavis@joegalaxy.com>
2022-01-11 15:06:36 -0600

    Implemented Creating New Booking on Checkout Success

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -1,17 +1,21 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const Tour = require('../models/tourModel');
+const Booking = require('../models/bookingModel');
 const catchAsync = require('../utils/catchAsync');
-const AppError = require('../utils/appError');
 const factory = require('./handlerFactory');
 
 exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   // 1 Get the currently booked tour.
   const tour = await Tour.findById(req.params.tourId);
-  console.log(tour);
+  //console.log(tour);
   // 2 Create checkout session (session info & product info)
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ['card'],
-    success_url: `${req.protocol}://${req.get('host')}/`,
+    //adding these buddy params on is not secure, tour, booking, price.
+    //because anyone who knows this ur could book a tour without paying. temp workaround.
+    success_url: `${req.protocol}://${req.get('host')}/?tour=${
+      req.params.tourId
+    }&user=${req.user.id}&price=${tour.price}`,
     cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
     customer_email: req.user.email,
     client_reference_id: req.params.tourId,
@@ -32,4 +36,14 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
     status: 'success',
     session,
   });
+});
+
+exports.createBookingCheckout = catchAsync(async (req, res, next) => {
+  const { tour, user, price } = req.query;
+
+  if (!tour && !user && !price) return next();
+  await Booking.create({ tour, user, price });
+
+  //create a new request. will hit viewRoute for '/'. temporary solution.
+  res.redirect(req.originalUrl.split('?')[0]);
 });
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const viewsController = require('../controllers/viewsController');
 const authController = require('../controllers/authController');
+const bookingController = require('../controllers/bookingController');
+
 const router = express.Router();
 
 // router.get('/', (req, res) => {
@@ -15,8 +17,14 @@ const router = express.Router();
 //put in the middleware stack for each and every route.
 //changing to only call isLoggedIn for routes that aren't protected instead of every route.
 //router.use(authController.isLoggedIn);
-
-router.get('/', authController.isLoggedIn, viewsController.getOverview);
+// noted add createBookingCheckout to '/' route because this is the route we'll hit on successful
+// stripe processing. this is tempoary.
+router.get(
+  '/',
+  bookingController.createBookingCheckout,
+  authController.isLoggedIn,
+  viewsController.getOverview
+);
 router.get('/tour/:slug', authController.isLoggedIn, viewsController.getTour);
 router.get('/login', authController.isLoggedIn, viewsController.getLoginForm);
 router.get('/me', authController.protect, viewsController.getAccount);
