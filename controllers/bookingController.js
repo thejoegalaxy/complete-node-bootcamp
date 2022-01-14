f08d1f5fbdfbd8929a544ecb2b536eb55f4659f1
C Davis <cdavis@joegalaxy.com>
2022-01-13 19:40:02 -0600

    improve stripe

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -56,7 +56,10 @@ const createBookingCheckout = async (session) => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
   const price = session.line_items[0].amount / 100;
+  console.log('✨✨✨✨✨');
   console.log(tour, user, price);
+  console.log('✨✨✨✨✨');
+
   await Booking.create({ tour, user, price });
 };
 
